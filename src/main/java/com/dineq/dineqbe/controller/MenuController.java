251d46bf6a26a96da8d525b083213854516079d7
dgsim <dgsim126@gmail.com>
2025-04-02 16:37:57 +0900

    feat: show all menu

--- src/main/java/com/dineq/dineqbe/controller/MenuController.java
+++ src/main/java/com/dineq/dineqbe/controller/MenuController.java
@@ -1,4 +1,32 @@
 package com.dineq.dineqbe.controller;
 
+import com.dineq.dineqbe.dto.menu.MenuResponseDTO;
+import com.dineq.dineqbe.service.MenuService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/store")
 public class MenuController {
+
+    private MenuService menuService;
+
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+    /**
+     * 모든 메뉴 조회
+     * GET /api/v1/store/menus
+     * @return
+     */
+    @GetMapping("/menus")
+    public ResponseEntity<List<MenuResponseDTO>> getAllMenus() {
+        List<MenuResponseDTO> menus= menuService.getAllMenu();
+        return ResponseEntity.ok(menus);
+    }
 }
--- src/main/java/com/dineq/dineqbe/service/MenuService.java
+++ src/main/java/com/dineq/dineqbe/service/MenuService.java
@@ -1,5 +1,34 @@
 package com.dineq.dineqbe.service;
 
+import com.dineq.dineqbe.dto.menu.MenuResponseDTO;
+import com.dineq.dineqbe.repository.MenuRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class MenuService {
 
+    private final MenuRepository menuRepository;
+
+    public MenuService(MenuRepository menuRepository) {
+        this.menuRepository = menuRepository;
+    }
+    
+    // 모든 메뉴 조회
+    public List<MenuResponseDTO> getAllMenu() {
+        return menuRepository.findAll().stream()
+                .map(menu -> new MenuResponseDTO(
+                        menu.getMenuId(),
+                        menu.getMenuName(),
+                        menu.getMenuPrice(),
+                        menu.isOnSale(),
+                        menu.getMenuPriority(),
+                        menu.getMenuInfo(),
+                        menu.getMenuImage(),
+                        menu.getCategory().getCategoryId() // CategoryEntity가 연결된 경우
+                ))
+                .collect(Collectors.toList());
+    }
 }
