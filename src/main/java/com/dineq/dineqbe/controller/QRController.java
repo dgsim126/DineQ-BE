71edbc652e48b77448fb09f088719feda028f4e4
donggeun <dgsim126@gmail.com>
2025-04-16 20:06:19 +0900

    before

--- src/main/java/com/dineq/dineqbe/controller/QRController.java
+++ src/main/java/com/dineq/dineqbe/controller/QRController.java
@@ -3,10 +3,7 @@ package com.dineq.dineqbe.controller;
 import com.dineq.dineqbe.service.QRService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/register")
