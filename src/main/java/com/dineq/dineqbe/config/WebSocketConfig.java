ea8c6b0c268a062bdead7869c15f401aa385648a
donggeun <dgsim126@gmail.com>
2025-09-24 23:13:42 +0900

    feat: socket allow origin converted

--- src/main/java/com/dineq/dineqbe/config/WebSocketConfig.java
+++ src/main/java/com/dineq/dineqbe/config/WebSocketConfig.java
@@ -15,13 +15,22 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      * 클라이언트가 최초로 접속할 웹소켓 엔드포인트(URL) 등록
      * @param registry
      */
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        // 프론트에서 ws://localhost:8080/ws 로 연결
+//        registry.addEndpoint("/ws")
+//                .setAllowedOriginPatterns("*"); // CORS 개념과 유사. 어떤 Origin에서 오는 핸드셰이크 요청을 허용할지 (로컬)
+//                // .setAllowedOriginPatterns("*"); // 실제 운영 시 특정 도메인만 허용하도록 변경할 것
+//    }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 프론트에서 ws://localhost:8080/ws 로 연결
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*"); // CORS 개념과 유사. 어떤 Origin에서 오는 핸드셰이크 요청을 허용할지 (로컬)
-                // .setAllowedOriginPatterns("*"); // 실제 운영 시 특정 도메인만 허용하도록 변경할 것
+                .setAllowedOriginPatterns(
+                        "https://dine-q-fe-renewer.vercel.app",
+                        "https://dineqws.duckdns.org"
+                );
     }
+
 
     /**
      * STOMP 목적지 체계 설정, 메시지를 어디로 보내고 받는지 주소 규칙 정하기
